0f01fa4b64650e68efac9f9f4a77a9cab2af78c9
AlexUshka <oleksandr.yushchenko1@gmail.com>
2020-11-19 14:56:13 +0200

    19.11.2020

--- TWO_BOTTON_MAIN/temp_control.ino
+++ TWO_BOTTON_MAIN/temp_control.ino
@@ -18,26 +18,35 @@ void temperature_control(){
 
     TempDS18B20 = ((data1[1] << 8) | data1[0]) * 0.0625;
 
-  Serial.println(TempDS18B20);
-  Serial.println(press1);
-  Serial.println(press10);
-  Serial.println(press11);
-  Serial.println(press12);
+  //Serial.println(TempDS18B20);
+  //Serial.println(press1);
+  //Serial.println(press10);
+  //Serial.println(press11);
+  //Serial.println(flag_100_xtime);
  }
-  if(TempDS18B20 < 30.50){                         // отпускание защиты при падении температуры до заданной
+  if(TempDS18B20 < 29.50){                         // отпускание защиты при падении температуры до заданной
     temp_control = 0;
   }
  
-  if(TempDS18B20 > 30.50){                            // срабатывание датчика при заданной температуре
+  if(TempDS18B20 > 29.50){                            // срабатывание датчика при заданной температуре
      temp_control = 1;
   }
-  if(temp_control && press1){      //|| press2 || press3)
+  if(temp_control && press1){                         // контроль температуры при нажатии на кнопку1
     analogWrite(led1,10); 
+    
    // brightness1 = 10;
   }
-  if(temp_control && press10){        //|| press2 || press3
+  if(temp_control && press10){                            // контроль температуры при нажатии на кнопку2
     analogWrite(led2,10);
+    
   //  brightness2 = 10;   
+  }
+  if(temp_control && flag_100_xtime){                     // отключаем режим 100% х время
+    analogWrite(led1,10); analogWrite(led2,10);
+    but13 = 1;
+  }
+  if(temp_control && press11){                            // отключаем реджим 100%
+    analogWrite(led1,10); analogWrite(led2,10);
   }
  // -------------------------------- контроль температуры --------------------------- 
 }                                            
